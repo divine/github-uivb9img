235213d4f488f12c228940e6d407e6c6906522d5
divine <git@stackblitz.com>
2025-01-27 20:30:34 -0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <button @click="download"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { saveAs } from 'file-saver';
+
+function download(){
+  saveAs();
+}
+
+</script>
